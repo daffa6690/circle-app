f3cb5b167047e981bf04ef96bcb46ed75f2f828a
daffa6690 <daffadaffamdf@gmail.com>
2025-07-02 12:59:59 +0700

    remove unuseable modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,16 @@
-/// <reference types="vitest" />
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: {
-    host: '0.0.0.0',
+    host: 'localhost',
     port: 5173,
-    allowedHosts: ['circle.local'],
-    proxy: {
-      '/api': {
-        target: 'http://192.168.18.175:2121',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-
-  }, 
+  },
   resolve: {
     alias: {
       '@': '/src',
-    },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
-    coverage: {
-      reporter: ['text', 'json', 'html'],
-      exclude: ['node_modules/', 'src/test/**/*'],
     },
   },
 });
